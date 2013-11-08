fda5e98f1e189073f688d38a5f1d101a990b3f55
Louisa Barrett <louisabarrett@gmail.com>
2013-11-08 14:22:23 -0700

    first commit - finishes i0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Merchant::Application.config.secret_key_base = 'e4f7707846a1e9a545bd3581550e079ef790d8993c102e1b24ec26dc67a6171b1873f9fd2420423941249b899f959c3e115584912b1ba10f25e21c38fb386a3f'
